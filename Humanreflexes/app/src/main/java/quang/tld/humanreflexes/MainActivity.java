a1a2cf34bf2865d2d9c7f13bd72846c05f209934
quangtldcc25tth-cell <quang.tld.cc25tth@ntu.edu.vn>
2026-08-25 00:41:39 +0700

    khai báo dữ liệu và kết nối giao diện XML với code Java

--- Humanreflexes/app/src/main/java/quang/tld/humanreflexes/MainActivity.java
+++ Humanreflexes/app/src/main/java/quang/tld/humanreflexes/MainActivity.java
@@ -1,24 +1,33 @@
 package quang.tld.humanreflexes;
-
+    
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ConstraintLayout layoutMain;
+    private TextView tvStatus;
+    private enum GameState { START, WAITING, READY }
+    private GameState currentState = GameState.START;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        ViewCompat.setOnApplyWindowInsetsListener(layoutMain, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        layoutMain = findViewById(R.id.layoutMain);
+        tvStatus = findViewById(R.id.tvStatus);
     }
 }
